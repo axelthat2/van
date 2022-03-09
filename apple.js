4e101b77426a9c66736c8ef4c91667a098c41124
sanjay <jadems321@gmail.com>
2022-03-09 17:52:59 +0545

    s

---
+++ apple.js
@@ -0,0 +1,8 @@
+export class Apple {
+  #a = 1
+  #b = 2
+
+  add() {
+    return a + b
+  }
+}
--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-import './style.css'
+import * as Sentry from "@sentry/browser"
+import { BrowserTracing } from "@sentry/tracing"
+import { Apple } from "./apple"
+import "./style.css"
 
-document.querySelector('#app').innerHTML = `
+Sentry.init({
+  dsn: "https://c39265164cb34e148a5d73a138271d94@o1098027.ingest.sentry.io/6250696",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
+document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+new Apple().add()
