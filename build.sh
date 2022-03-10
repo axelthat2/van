7f8037a3b6c8c89b9deb4c3e9eba07d17d8aab16
sanjay <jadems321@gmail.com>
2022-03-10 12:22:18 +0545

    gp

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ sentry-cli $release new vance-dance --finalize
 
 sentry-cli $release files vance-dance upload-sourcemaps ./dist/assets/*.map
 
-for path in ./dist/assets/*.js; do
-  sed -i '$ d' "$path"
-done
+# for path in ./dist/assets/*.js; do
+#   sed -i '$ d' "$path"
+# done
 
 rm -rf ./dist/assets/*.map
 
