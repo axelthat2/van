206e8822ef3fb9fc0a9e965bbb1a6b38e5f4945f
sanjay <jadems321@gmail.com>
2022-03-10 12:20:12 +0545

    h

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ yarn build
 
 release="--auth-token ${SENTRY_AUTH_TOKEN} releases --org cashforcarsio --project javascript"
 
-# sentry-cli $release files vance-dance delete --all
+sentry-cli $release files vance-dance delete --all
 
 sentry-cli $release new vance-dance --finalize
 
-sentry-cli $release files vance-dance upload-sourcemaps ./dist/assets
+sentry-cli $release files vance-dance upload-sourcemaps ./dist/assets/*.map
 
 for path in ./dist/assets/*.js; do
   sed -i '$ d' "$path"
